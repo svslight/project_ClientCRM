ef92e55a38e2e485dcc4a381350b4a1c5b6f36a4
Svetlana Solodovnikova <svslight@yandex.ru>
2021-03-08 09:41:43 +0500

    Added resources client_statuses

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
 
   match '/countries/cancel', to: 'countries#cancel', via: 'get'
   
-  resources :clients, :countries, :client_statuses  
+  resources :clients, :countries, :client_statuses
 end
