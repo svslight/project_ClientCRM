df016a33884ec0d99592706bf805a5b56f28e49e
Svetlana Solodovnikova <svslight@yandex.ru>
2021-03-29 12:55:28 +0500

    Refactoring

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,7 +8,12 @@ class ClientsController < ApplicationController
 
   def create
     if client.save
-      @user=User.create(client_id: client.id, email: client.email, password: '123456', password_confirmation: '123456' ) if params[:client][:make_user]
+
+      status_client_append(client)
+
+      params[:client][:make_user] ? @user = User.create(client_id: client.id, email: client.email, password: '123456', password_confirmation: '123456' )
+        : client.user
+
       redirect_to client_path(client), notice: 'Client was successfully created'
     else
       render :new
@@ -17,11 +22,13 @@ class ClientsController < ApplicationController
 
   def update
     client.update(client_params)
-    if params[:client][:make_user]
-      @user=User.create(client_id: client.id, email: client.email, password: '123456', password_confirmation: '123456' ) if !User.where(client_id: client.id).exists?
-    else
-      User.find_by(client_id: client.id).destroy if User.where(client_id: client.id).exists?
-    end
+
+    status_client_append(client)
+
+    !params[:client][:make_user] && user_exists?(client) ? client.user.destroy
+      : User.create(client_id: client.id, email: client.email, password: '123456', password_confirmation: '123456')
+ 
+    redirect_to clients_path
   end
 
   def destroy
@@ -31,14 +38,31 @@ class ClientsController < ApplicationController
 
   private
 
+  def status_client_append(client)
+    client.status_clients.each{ |s| s.destroy }   
+    @ids = params[:client][:ids].to_s.split(/\s/)
+    @ids.each do |id|
+      status_client = StatusClient.new
+      status_client.client = client
+      status_client.status = Status.find(id.to_i)
+      status_client.save
+    end
+  end
+  
+  def user_exists?(client)
+    User.where(client_id: client).exists?
+  end
+
   def load_client
     @client = Client.find(params[:id])
   end
 
   def client_params
-    params.require(:client).permit(:name, :surname, :patronymic, :birthdate, :city, :address, 
-                                    :phon, :skype, :entry_date, :lesson_number, :email,
-                                    :communicant_date, :comments, :country_id, :client_status_id,
-                                    :group_id, :group_position_id, :team_project_id, :team_position_id, :make_user)
+    params.require(:client).permit(:name, :surname, :patronymic, :birthdate, :city, :address,
+                                   :phon, :skype, :entry_date, :lesson_number, :email,
+                                   :communicant_date, :comments, :country_id, :client_status_id,
+                                   :group_id, :group_position_id, :team_project_id, :team_position_id,
+                                   :make_user, :ids)
   end
+
 end
