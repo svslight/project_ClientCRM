09c62d27e4a20d8b26350b99036b439591f4f37e
Svetlana Solodovnikova <svslight@yandex.ru>
2021-04-04 12:50:22 +0500

    Refactoring model group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,8 @@
 class Group < ApplicationRecord
   belongs_to :country, optional: true
   belongs_to :group_status, optional: true
+
   has_many :clients, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  it { should have_many(:clients).dependent(:destroy) }
+RSpec.describe Group, type: :model do  
   it { should belong_to(:country).optional }
   it { should belong_to(:group_status).optional }
+
+  it { should have_many(:clients).dependent(:destroy) }
+
+  it { should validate_presence_of :name }
 end
